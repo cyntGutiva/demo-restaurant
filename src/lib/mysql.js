678f05a99caf39f8837961f0b2762b03b5b84c81
cynthia <cynthiagutierrezvargas@gmail.com>
2026-03-20 17:15:34 -0300

    mejoras conexion a bd y carrito

---
+++ src/lib/mysql.js
@@ -0,0 +1,10 @@
+import mysql from 'mysql2/promise';
+
+export const pool = mysql.createPool({
+  host: import.meta.env.DB_HOST,
+  user: import.meta.env.DB_USER,
+  password: import.meta.env.DB_PASSWORD,
+  database: import.meta.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
